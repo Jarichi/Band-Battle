55025262aa1e18b086aeae71e2d3e5310cbbaf70
ChrisC-Fontys <c.carolus@student.fontys.nl>
2024-04-15 13:51:05 +0200

    added spawning for empty mic stand

--- Assets/Microphone.cs
+++ Assets/Microphone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
 
@@ -10,14 +11,19 @@ public class Microphone : MonoBehaviour
     public bool inRange = false;
     public bool isPlaying = false;
     public bool inCombat = false;
+    private bool spawned = false;
     public BoxCollider2D tr;
     public Animator animator;
+
+    public GameObject emptyInstrument;
+
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         tr = GetComponent<BoxCollider2D>();
+        
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -37,6 +43,7 @@ public class Microphone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //check if in range of the object (player hitbox intersects the trigger hitbox)
         if (inRange) {
             if(Input.GetKeyDown(KeyCode.U))
             {
@@ -45,17 +52,31 @@ public class Microphone : MonoBehaviour
 
             }
 
+            //check if is playing the instrument currently
             if (isPlaying) {
+
+                //spawn how the object looks while playing the instrument ONCE
+                while (!spawned)
+                {
+                    Instantiate(emptyInstrument, new Vector2(0, 2), Quaternion.identity);
+                    spawned = true;
+
+                }
+
+                //check if you want to enter combat mode
                 if(Input.GetKeyDown(KeyCode.I))
-            {
-                isPlaying = false;
-                inCombat = true;
-                animator.SetBool("IsWeapon", inCombat);
-                Debug.Log(inCombat);
-            }
+                {
+                    isPlaying = false;
+                    inCombat = true;
+                    animator.SetBool("IsWeapon", inCombat);
+                    Debug.Log(inCombat);
+                }
             }
 
             
         }
+
+
+
     }
 }
