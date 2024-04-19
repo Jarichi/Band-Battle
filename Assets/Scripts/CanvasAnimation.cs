af84cf1f1ff993b7dcc0b6e1c8729a64aa1a45a9
ChrisC-Fontys <c.carolus@student.fontys.nl>
2024-04-19 15:04:26 +0200

    added playing gui for guitar and mic, should be expandable later for other animations

---
+++ Assets/Scripts/CanvasAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasAnimation : MonoBehaviour //might alse need to inherit the player class to disable animation when combat
+{
+
+    private Animator GUIAnimation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GUIAnimation = GetComponent<Animator>();
+        //GUIAnimation.enabled = false;
+        PlayAnimation();
+    }
+
+
+    void PlayAnimation()
+    {
+        //play animation ONLY after the can vast gets enabled.
+        // GUIAnimation.enabled = true;
+        GUIAnimation.SetTrigger("GuitarGUI");
+
+
+        
+    }
+
+
+}
