1d0188d42ae3af38c5a48dcbe33050ea0c1f4642
ChrisC-Fontys <c.carolus@student.fontys.nl>
2024-04-22 12:35:05 +0200

    added particle effects to dummy script. TODO: fix particles only spawning once

--- Assets/Scripts/Player/DummyCombat.cs
+++ Assets/Scripts/Player/DummyCombat.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class DummyCombat : MonoBehaviour, IDamageable
 {
     public Animator animator;
+    public int Hitpoints;
+    [SerializeField] private ParticleSystem DamageParticles;
+
+    private ParticleSystem DamageParticleInstance;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();   
+        animator = GetComponent<Animator>();  
+        //DamageParticles = GetComponent<ParticleSystem>(); 
+
     }
 
     // Update is called once per frame
@@ -21,5 +28,15 @@ public class DummyCombat : MonoBehaviour, IDamageable
     public void OnDamage()
     {
         animator.SetTrigger("Hit");
+        SpawnParticles();
+        Hitpoints--;
+        
+    }
+
+    private void SpawnParticles()
+    {
+        //create particles at the current position.
+        DamageParticles = Instantiate(DamageParticles, transform.position, Quaternion.identity);
+        
     }
 }
