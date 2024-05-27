c2ce15876a242f773904bdd80c9b69924da06175
Towo <towodileofficial@gmail.com>
2024-05-27 13:00:07 +0200

    MIDI2Beatmap works!
    Will always create out.bbm (will overwrite itself) so be careful

--- Assets/Scripts/Utility/MIDI2Beatmap.cs
+++ Assets/Scripts/Utility/MIDI2Beatmap.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,11 +7,12 @@ using Melanchall.DryWetMidi.Interaction;
 using Melanchall.DryWetMidi.MusicTheory;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using static Beatmap;
 
 public class MIDI2Beatmap : EditorWindow
 {
+    private string outputPath = "Assets/Scripts/Minigame/Beatmap/";
     private string midiFilePath = "Assets/Audio/MIDI/";
 
     [MenuItem("Tools/MIDI to Beatmap converter")]
@@ -24,6 +25,8 @@ public class MIDI2Beatmap : EditorWindow
     {
         GUILayout.Label("MIDI file path", EditorStyles.boldLabel);
         midiFilePath = EditorGUILayout.TextField(midiFilePath);
+        GUILayout.Label("Output file path", EditorStyles.boldLabel);
+        outputPath = EditorGUILayout.TextField(outputPath);
         if (GUILayout.Button("Convert"))
         {
             Convert();
@@ -38,16 +41,72 @@ public class MIDI2Beatmap : EditorWindow
 
         var notes = midi.GetNotes();
 
-        for (int i = 0; i < 4; i++)
+        var channels = new List<Channel>();
+        double tempo = 0f;
+        for (int i = 0; i < midi.GetChannels().Count(); i++)
         {
+            List<NotePosition> positions = new List<NotePosition>();
             foreach (var note in notes)
             {
                 if (note.Channel == i)
                 {
                     var time = note.TimeAs<BarBeatFractionTimeSpan>(map);
-                    Debug.Log("CHANNEL : " + note.Channel + "| NOTE : " + note.NoteName + "| BAR : " + time.Bars + "| BEAT : " + time.Beats);
+                    tempo = midi.GetTempoMap().GetTempoAtTime(time).BeatsPerMinute;
+                    var beatTotal = (time.Bars * 4) + time.Beats;
+                    positions.Add(new NotePosition(note.NoteNumber % 4, beatTotal));
+                    //Debug.Log("CHANNEL : " + note.Channel + "| NOTE : " + note.NoteName + "| BAR : " + time.Bars + "| BEAT : " + time.Beats);
                 }
             }
+            channels.Add(new Channel(i, positions));
         }
+        var beatmap = new Beatmap(tempo, channels);
+        Debug.Log(beatmap.Channels.Count);
+        string jsonString = JsonUtility.ToJson(beatmap, true);
+        var path = outputPath + "out.bbm";
+        if (File.Exists(path))
+            File.CreateText(path);
+        else 
+            File.WriteAllText(path, jsonString);
+        Debug.Log(jsonString);
+    }
+}
+
+[Serializable]
+public class Beatmap
+{
+    public double BPM;
+    [SerializeField]
+    public List<Channel> Channels;
+
+    public Beatmap(double bpm, List<Channel> channels) { 
+        BPM = bpm;  Channels = channels; 
+    }
+    
+}
+
+[Serializable]
+public class Channel
+{
+    public int Number;
+    [SerializeField]
+    public List<NotePosition> Positions;
+
+    public Channel(int number, List<NotePosition> positions)
+    {
+        this.Number = number;
+        this.Positions = positions;
+    }
+}
+
+[Serializable]
+public class NotePosition
+{
+    public int Direction;
+    public double Beat;
+
+    public NotePosition(int direction, double beat)
+    {
+        this.Direction = direction;
+        this.Beat = beat;
     }
 }
